b574950f6a790dc26752d1cac160a70a0540d403
navarrojandg <42248628+navarrojandg@users.noreply.github.com>
2018-12-29 23:13:27 -0800

    initial commit
    added multithread functionality

--- create_linode.py
+++ create_linode.py
@@ -1,9 +1,7 @@
 from linode_api4 import LinodeClient
 from config import params
+from multiprocessing.dummy import Pool as ThreadPool
 
-API_KEY = params['API_KEY']
-AUTH_USER = params['AUTH_USER']
-AUTH_PASS = params['AUTH_PASS']
 
 class NewProxy():
     def __init__(self, region, ltype):
@@ -13,57 +11,52 @@ class NewProxy():
         self.image = "linode/ubuntu16.04lts"
         self.stackscript = 68166
 
+    def create(self):
+        new_linode, password = self.client.linode.instance_create(
+            self.ltype,
+            self.region,
+            image=self.image,
+            stackscript=self.stackscript,
+            stackscript_data={
+                "squid_user":"{}".format(AUTH_USER),
+                "squid_password":"{}".format(AUTH_PASS)
+            }
+        )
+        with open('LINODE PROXY LIST.txt', 'a+') as f:
+            f.write('{}:{}:{}:{}\n'.format(
+                new_linode.ipv4[0],
+                3128,
+                AUTH_USER,
+                AUTH_PASS
+            ))
+        with open('LINODE PROXY PASSWORDS.txt', 'a+') as f:
+            f.write('{} \t{}\n'.format(new_linode.ipv4[0], password))
+        return '{}:3128:{}:{}'.format(new_linode.ipv4[0], AUTH_USER, AUTH_PASS)
 
-class NewProxy():
-    def __init__(self):
-        self.API_KEY = API_KEY
-        self.client = LinodeClient(API_KEY)
-        self.region = None
-        self.ltype = None
-        self.image = "linode/ubuntu16.04lts"
-        self.stackscript = 68166
-        self.count = raw_input('How many proxies would you like to make?: ')
-        self.set_region()
-        self.set_type()
 
-    def set_region(self):
-        region_list = self.client.regions()
-        for i in range(len(region_list)):
-            print('{} {}'.format(i, region_list[i]))
-        r = input('Please select an available region: ')
-        self.region = region_list[r]
-
-    def set_type(self):
-        type_list = self.client.linode.types()
-        for i in range(len(type_list)):
-            print('{} {}'.format(i, type_list[i]))
-        t = input('Please select an available linode type: ')
-        self.ltype = type_list[t]
-
-    def create_linodes(self):
-        for i in range(int(self.count)):
-            new_linode, password = self.client.linode.instance_create(
-                self.ltype,
-                self.region,
-                image=self.image,
-                stackscript=self.stackscript,
-                stackscript_data={
-                    "squid_user":"{}".format(AUTH_USER),
-                    "squid_password":"{}".format(AUTH_PASS)
-                }
-            )
-            print('Creating Proxy {}...'.format(new_linode.ipv4[0]))
-            with open('LINODE PROXY LIST.txt', 'a+') as f:
-                f.write('{}:{}:{}:{}\n'.format(
-                    new_linode.ipv4[0],
-                    3128,
-                    AUTH_USER,
-                    AUTH_PASS
-                ))
-            with open('LINODE PROXY PASSWORDS.txt', 'a+') as f:
-                f.write('{} \t{}\n'.format(new_linode.ipv4[0], password))
+def set_region():
+    region_list = LinodeClient(API_KEY).regions()
+    for i in range(len(region_list)):
+        print('{} | {}'.format(i, region_list[i]))
+    r = input('Please select an available region: ')
+    return region_list[r]
 
 
 if __name__ == '__main__':
-    my_proxies = NewProxy()
-    my_proxies.create_linodes()
+    #EDIT PARAMS IN CONFIG FILE FIRST
+    API_KEY = params['API_KEY']
+    AUTH_USER = params['AUTH_USER']
+    AUTH_PASS = params['AUTH_PASS']
+    #USER SETS QTY AS WELL AS REGION, LINODE TYPE IS AUTO SET TO NANODE
+    count = int(input("How many proxies would you like to create? "))
+    region = set_region()
+    print("CREATING {} | {} PROXIES PLEASE WAIT".format(count, region))
+    ltype = LinodeClient(API_KEY).linode.types()[0]
+    proxylist = []
+    for i in range(count):
+        proxylist.append(NewProxy(region, ltype))
+    pool = ThreadPool()
+    proxies = pool.map(lambda x: x.create(), proxylist)
+    for p in proxies:
+        print(p)
+    print("Proxy list has been exported to LINODE PROXY LIST.txt")
